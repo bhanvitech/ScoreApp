0d29b67f0b3fa8d6fb50d8a125ec562441e4b81f
Bhanvi Dudeja <100353170+bhanvitech@users.noreply.github.com>
2022-05-03 12:10:17 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React  from 'react'
+// import UseEffect from './component/hooks/useEffect';
+import UseReducer from './component/hooks/useReducer';
+
+// import UseState from "./component/hooks/useState";
+
+const App = () => {
+  return (
+    <>
+    
+     <UseReducer/>
+     </>
+  )
+}
+
+export default App;
+ 
